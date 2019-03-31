7f532f3f018750d7cbea8cb10b753c872159a663
Seth Treviño <sethtv@hotmail.com>
2019-03-30 18:52:13 -0600

    Actualizacion

---
+++ Exno-Construccion/index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="keywords" content="consultoria,contabilidad,fiscal,coaching empresarial guadalajara,coach de negocios guadalajara,coaching de negocios,coaching para empresas,negocios,asesoria fiscal guadalajara,contadores en guadalajara,consultores de negocios guadalajara">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Consultores de Negocios. Contabilidad General y Financiera, Asesoria Fiscal, Derecho corporativo y mercantil.               
+  ¿Tienes alguna pregunta? 
+  Estamos a tus servicios Contactanos
+  (01) 33 3813 - 0258
+  &copy; Todos los derechos reservados a Grupo Exno">
+  <meta name="author" content="Sethvices">
+  <title>Grupo Exno</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <!--<link rel="stylesheet" type="text/css" href="../Exno/css/modal-video.min.css"> -->
+  
+  <!-- Bootstrap -->
+  <link href="../Exno/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../Exno/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../Exno/css/animate.css">
+  <link href="../Exno/css/prettyPhoto.css" rel="stylesheet">
+  <link href="../Exno/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/../Exno/css/font-awesome.min.css">
+
+  <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        </div>
+        <div class="modal-body">
+          <iframe id="video" width="100%" height="500px" frameborder="10" ></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+ 
+</head>
+
+<body>
+  <header>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+
+            <a href="#" class="pull-left">
+              <img style=" width: 245px; height: 80px; " src="images/LOGO_EXNO.jpg">
+            </a>
+            <!--<a href="index.php"><h1><span>Tu</span>Logo</h1></a>-->
+
+          </div>
+
+          <div class="navbar-collapse collapse">
+            <div class="menu">
+              <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation">
+                  <a href="index.php" class="active">Inicio</a>
+                </li>
+                <li role="presentation">
+                  <a href="about.html">Sobre Nostros</a>
+                </li>
+               <!--   <li role="presentation">
+                  <a href="services.html">Servicios</a>
+                </li> -->
+                <li role="presentation">
+                  <a href="portfolio.php">Portafolio</a>
+                </li>
+                <li role="presentation">
+                  <a href="contact.html">Contacto</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <nav class="nav-wh" style="padding-top: 60px;margin-top: 0; background-color:#243743; ">
+    <div class="container">
+      <div class="col-sm-4">
+        <ul class="social-network" style="height: 30px;">
+
+
+        </ul>
+      </div>
+      <div class="col-md-4 col-md-offset-4">
+        <div class="copyright" style="font-size:18px;">
+          <a href="#" class="ytube tool-tip" title="WhatApp">
+            <i class="fa fa-phone"> </i>
+          </a>
+          <a> </a>
+          (01) 33 3813 - 0258
+        </div>
+      </div>
+    </div>
+
+  </nav>
+
+  
+
+  <div id="myCarousel" class="carousel slide" style="margin-top: 0;" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img  src="images/slider/bg1.jpg" class="item1" alt="Los Angeles" style=" height: 640px; width:100%">
+        <div  class="carousel-caption">
+            <p class="carousel-p">Estamos comprometidos con las satisfacción del </p>
+          <h3 class="carousel-h3" >Cliente</h3>
+        </div>
+      </div>
+
+      <div class="item">
+        <img  src="images/slider/bg3.jpg" class="item1" alt="Chicago" style="  height: 640px;width:100%;">
+        <div class="carousel-caption">
+        <p class="carousel-p">Especializados en </p>
+          <h3 class="carousel-h3" >EDIFICACIÓN</h3>
+
+        </div>
+      </div>
+
+      <div class="item">
+        <img  src="images/slider/bg2.jpg" class="item1" alt="New York" style=" height: 640px; width:100%;">
+        <div class="carousel-caption">
+          <p class="carousel-p">La base de nuestro éxito radica</p>
+          <h3 class="carousel-h3" >En el enfoque global, practico y humano, con el que contamos</h3>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+  </div>
+
+
+
+  <div class="about">
+    <div class="container">
+      <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+
+        <img src="images/6.jpg" class="img-responsive" />
+      </div>
+
+      <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <h2>Misión</h2>
+        <p class="p" style="font-size: 17px;">
+            En Exno Contrucción buscamos siempre aportar satisfacción y valor para los negocios de nuestros clientes y para los objetivos de nuestros colaboradores.  La base de nuestro éxito radica en el  enfoque global, práctico y humano con el que contamos; el cual abarca diversas especialidades de la ingeniería civil, lo que nos permite comprometernos de una manera permanente en relaciones de ganar – ganar.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="lates">
+    <div class="container">
+        <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <h2>Visión</h2>
+            <p class="p" style="font-size: 17px;">
+                Exno Contrucción se ve como líder en la prestación de servicios de construcción, y mantenimiento de obras civiles comerciales, industriales y habitacionales abarcando en el corto plazo la región del pacifico y posteriormente todo el territorio nacional.
+            </p>
+          </div>
+      <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <img src="images/2.jpg" class="img-responsive" />
+      </div>
+
+    
+    </div>
+  </div>
+
+  <div class="about">
+      <div class="container">
+        <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="300ms">
+  
+          <img src="images/7.jpg" class="img-responsive" />
+        </div>
+  
+        <div class="col-md-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <h2>Nuestros Objetivos              </h2>
+            <p class="p" style="font-size: 17px;">
+                En Exno Contrucción estamos comprometidos con la satisfacción del cliente y de nuestro personal, promoviendo una cultura de prevención y de mejora continua en nuestros procesos.   
+            </p>
+          </div>
+      </div>
+    </div>
+
+    </div>
+
+  
+ 
+    <section id="portfolio">
+        <div class="container">
+          <!-- <div class="center">
+            <p>Nuestros Clientes</p>
+          </div>
+    -->
+    <br>
+          <ul class="portfolio-filter text-center">
+            <li><a class="btn btn-default active" href="#" data-filter="*">Nuestros Clientes</a></li>
+    </div>
+    
+        <div class="container">
+          
+          <div class="">
+            <div class="portfolio-items">
+            <?php
+                    $all_files = glob("images/clientes/*.*");
+                    for ($i=0; $i<count($all_files); $i++)
+                        {
+                        $image_name = $all_files[$i];
+                        $supported_format = array('gif','jpg','jpeg','png','JPG');
+                        $ext = strtolower(pathinfo($image_name, PATHINFO_EXTENSION));
+                        if (in_array($ext, $supported_format))
+                            {
+                                echo   '<div class="portfolio-item apps col-xs-12 col-sm-4 col-md-3">
+                                <div class="recent-work-wrap">
+                                  <img class="img-responsive" style="height: 150px; width: 250px; margin: 0 auto;" src="'.$image_name .'" alt="'.$image_name.'">
+                                  <div class="overlay">
+                                    <div style="margin-right: 14px; "class="recent-work-inner"> 
+                                   </div>
+                                  </div>
+                                </div>
+                              </div>';
+                            
+                            } else {
+                                continue;
+                            }
+                        }
+                            
+                        ?>
+        
+              
+    
+        </div>
+      </section>
+    
+    
+
+  <section name="Seth" id="conatcat-info" >
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="media contact-info wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+            <div class="pull-left">
+              <i class="fa fa-phone"></i>
+            </div>
+            <div class="media-body">
+              <h2>¿Tienes alguna pregunta?</h2>
+              <p>Estamos a tus servicios Contactanos</p>
+              <h3>(01) 33 3813 - 0258 </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/.container-->
+  </section>
+  <!--/#conatcat-info-->
+  <footer>
+    <div class="footer">
+      <div class="container">
+        <div class="social-icon">
+          <div class="col-md-4">
+            <ul class="social-network">
+              <li>
+                <a href="https://www.facebook.com/grupoexno/?ref=br_rs" class="fb tool-tip" title="Facebook">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/grupoexno/" class="twitter tool-tip" title="Instagram">
+                  <i class="fa fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+       
+      
+        <div class="col-md-4 col-md-offset-4">
+          <div class="copyright">
+            &copy; Todos los derechos reservados a Grupo Exno
+            <div class="credits">
+              <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Company            
+              <a href="https://bootstrapmade.com/bootstrap-business-templates/">Bootstrap Business Templates</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+    
+      
+    </div>
+  </footer>
+
+ <script>
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    if(check == true){
+      var y =  document.getElementsByClassName('carousel-p');
+      var z =  document.getElementsByClassName('item1');
+      var r =  document.getElementsByClassName('carousel-h3');
+      var d =  document.getElementsByClassName('item');
+      var i;
+      for (i = 0; i < y.length; i++) {
+         y[i].style = 'color: #FFFFFF; font-size:20px; margin-top:80px;  text-shadow: 1px 1px 1px  black;';
+         z[i].style =" height: 300px; margin-top:80px; width:100%";
+         d[i].style =" height: 300px;  width:100%";
+         r[i].style ="color: #fff; text-transform:uppercase; font-size:30px; font-weight:800; text-shadow: 1px 1px 1px   #243743;  ";
+     }
+    }
+ 
+ </script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="../Exno/js/jquery.min.js"></script>
+  <script src="../Exno/js/jquery-2.1.1.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="../Exno/js/bootstrap.min.js"></script>
+  <script src="../Exno/js/jquery.prettyPhoto.js"></script>
+  <script src="../Exno/js/jquery.isotope.min.js"></script>
+  <script src="../Exno/js/wow.min.js"></script>
+  <script src="../Exno/js/functions.js"></script>
+
+
+</body>
+
+</html>
---
+++ Exno-Construccion/portfolio.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="consultoria,contabilidad,fiscal,coaching empresarial guadalajara,coach de negocios guadalajara,coaching de negocios,coaching para empresas,negocios,asesoria fiscal guadalajara,contadores en guadalajara,consultores de negocios guadalajara">
+  <title>Grupo Exno</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+  <!-- Bootstrap -->
+  <link href="../Exno/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../Exno/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../Exno/css/animate.css">
+  <link href="../Exno/css/prettyPhoto.css" rel="stylesheet">
+  <link href="../Exno/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse.collapse">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+            <a href="#" class="pull-left">
+                <img style=" width: 245px; height: 80px;" src="images/LOGO_EXNO.jpg">
+              </a>
+          </div>
+
+          <div class="navbar-collapse collapse">
+            <div class="menu">
+              <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation"><a href="index.php" >Inicio</a></li>
+                <li role="presentation"><a href="about.html">Sobre Nostros</a></li>
+                    <!--   <li role="presentation">
+                  <a href="services.html">Servicios</a>
+                </li> -->
+                <li role="presentation"><a href="portfolio.php" class="active">Portafolio</a></li>
+                <li role="presentation"><a href="contact.html">Contacto</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div id="breadcrumb">
+    <div class="container">
+      <div class="breadcrumb">
+        <li><a href="index.php">Inicio</a></li>
+        <li>Portafolio</li>
+      </div>
+    </div>
+  </div>
+  
+  <div id="myCarousel" class="carousel slide" style="margin-top: 0;" data-ride="carousel">
+  
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img  src="images/slider/bg2.jpg" class="item1" alt="New York" style=" height: 640px; width:100%;">
+        <div class="carousel-caption">
+          <p class="carousel-p">Especializados en </p>
+          <h3 class="carousel-h3" >EDIFICACIÓN</h3>
+          <a type="submit" href="#portfolio" name="submit" class="btn btn-primary btn-lg" required="required">Ver Proyectos</a>
+        <br>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+  </div>
+
+
+  <section id="portfolio">
+    <div class="container">
+      <!-- <div class="center">
+        <p>Nuestros Clientes</p>
+      </div>
+-->
+<br>
+      <ul class="portfolio-filter text-center">
+        <li><a class="btn btn-default active" href="#" data-filter="*">Nuestros Proyectos</a></li>
+</div>
+
+    <div class="container">
+      
+      <div class="">
+        <div class="portfolio-items">
+        <?php
+                $all_files = glob("images/construccion/*.*");
+                for ($i=0; $i<count($all_files); $i++)
+                    {
+                    $image_name = $all_files[$i];
+                    $supported_format = array('gif','jpg','jpeg','png','JPG');
+                    $ext = strtolower(pathinfo($image_name, PATHINFO_EXTENSION));
+                    if (in_array($ext, $supported_format))
+                        {
+                            echo   '<div class="portfolio-item apps col-xs-12 col-sm-4 col-md-3">
+                            <div class="recent-work-wrap">
+                              <img class="img-responsive" style="height: 150px; width: 250px; margin: 0 auto;" src="'.$image_name .'" alt="'.$image_name.'">
+                              <div class="overlay">
+                                <div style="margin-right: 14px; "class="recent-work-inner"> 
+                               </div>
+                              </div>
+                            </div>
+                          </div>';
+                        
+                        } else {
+                            continue;
+                        }
+                    }
+                        
+                    ?>
+    
+          
+
+    </div>
+  </section>
+
+
+  <footer>
+    <div class="footer">
+      <div class="container">
+
+       
+      
+        <div class="col-md-4 col-md-offset-4">
+          <div class="copyright">
+            &copy; Todos los derechos reservados a Grupo Exno
+            <div class="credits">
+              <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Company            
+              <a href="https://bootstrapmade.com/bootstrap-business-templates/">Bootstrap Business Templates</a> by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+    </div>
+  </footer>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="../Exno/js/jquery-2.1.1.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="../Exno/js/bootstrap.min.js"></script>
+  <script src="../Exno/js/jquery.prettyPhoto.js"></script>
+  <script src="../Exno/js/jquery.isotope.min.js"></script>
+  <script src="../Exno/js/wow.min.js"></script>
+  <script src="../Exno/js/functions.js"></script>
+
+
+</body>
+
+</html>
